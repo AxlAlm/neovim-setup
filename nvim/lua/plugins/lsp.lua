65c02ddebf8a494bd2077484865780004e26b711
Axel Almquist <axel.almquist@flowertech.se>
2024-12-20 10:19:02 +0100

    move from lsp zero

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,131 +1,100 @@
 return {
-	"VonHeikemen/lsp-zero.nvim",
-	branch = "v3.x",
-	dependencies = {
+	{
 		"neovim/nvim-lspconfig",
-		"hrsh7th/cmp-nvim-lsp",
-		"hrsh7th/nvim-cmp",
-		"L3MON4D3/LuaSnip",
-		"williamboman/mason.nvim",
-		"williamboman/mason-lspconfig.nvim",
-		"WhoIsSethDaniel/mason-tool-installer.nvim",
-		"rafamadriz/friendly-snippets",
-	},
-	config = function()
-		local lsp_zero = require("lsp-zero")
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+			"williamboman/mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
+			"WhoIsSethDaniel/mason-tool-installer.nvim",
+		},
+		config = function()
+			local on_attach = function(_, bufnr)
+				local opts = { buffer = bufnr, remap = false }
+				vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+				vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+				vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+				vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+				-- diagnostics
+				vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
+				vim.keymap.set("n", "<leader>dn", vim.diagnostic.goto_next, opts)
+			end
 
-		lsp_zero.on_attach(function(client, bufnr)
-			local opts = { buffer = bufnr, remap = false }
-			vim.keymap.set("n", "gd", function()
-				vim.lsp.buf.definition()
-			end, opts)
-			vim.keymap.set("n", "K", function()
-				vim.lsp.buf.hover()
-			end, opts)
-			vim.keymap.set("n", "<leader>ca", function()
-				vim.lsp.buf.code_action()
-			end, opts)
-			vim.keymap.set("n", "<leader>rn", function()
-				vim.lsp.buf.rename()
-			end, opts)
-			vim.keymap.set("n", "<leader>d", function()
-				vim.diagnostic.open_float()
-			end, opts)
-		end)
-
-		local mason = require("mason")
-		local mason_lspconfig = require("mason-lspconfig")
-		local mason_tool_installer = require("mason-tool-installer")
-
-		mason.setup({
-			ui = {
-				icons = {
-					package_installed = "✓",
-					package_pending = "➜",
-					package_uninstalled = "✗",
+			-- Setup mason
+			require("mason").setup({
+				ui = {
+					icons = {
+						package_installed = "✓",
+						package_pending = "➜",
+						package_uninstalled = "✗",
+					},
 				},
-			},
-		})
+			})
 
-		local lang_servers = {
-			"pyright",
-			"gopls",
-			"ts_ls",
-			"html",
-			"cssls",
-			"tailwindcss",
-			"lua_ls",
-			"rust_analyzer",
-			"templ",
-		}
-
-		mason_lspconfig.setup({
-			ensure_installed = lang_servers,
-			automatic_installation = true,
-		})
-
-		mason_tool_installer.setup({
-			ensure_installed = {
-				"goimports", -- go import fomratter
-				"golines", -- go formatter for long lines
-				"prettierd", -- prettier formatter (for yaml, ts etc)
-				"stylua", -- lua formatter
-				"isort", -- python formatter
-				"black", -- python formatter
-				"ruff", -- python linter
+			local servers = {
+				"pyright",
+				"gopls",
+				"ts_ls",
 				"html",
+				"cssls",
+				"tailwindcss",
+				"lua_ls",
+				"rust_analyzer",
 				"templ",
-			},
-		})
+			}
 
-		local lspconfig = require("lspconfig")
+			-- Setup mason-lspconfig
+			require("mason-lspconfig").setup({
+				ensure_installed = servers,
+				automatic_installation = true,
+			})
 
-		lspconfig.html.setup({
-			on_attach = on_attach,
-			capabilities = capabilities,
-			filetypes = { "html", "templ" },
-		})
+			-- Setup mason-tool-installer
+			require("mason-tool-installer").setup({
+				ensure_installed = {
+					"goimports",
+					"golines",
+					"prettierd",
+					"stylua",
+					"isort",
+					"black",
+					"ruff",
+					"html",
+					"templ",
+				},
+			})
 
-		lspconfig.tailwindcss.setup({
-			on_attach = on_attach,
-			capabilities = capabilities,
-			filetypes = { "templ", "astro", "javascript", "typescript", "react" },
-			init_options = { userLanguages = { templ = "html" } },
-		})
+			-- Configure LSP servers
+			local lspconfig = require("lspconfig")
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-		vim.filetype.add({ extension = { templ = "templ" } })
+			-- Default configuration for all servers
+			local default_config = {
+				on_attach = on_attach,
+				capabilities = capabilities,
+			}
 
-		lspconfig.gopls.setup({
-			on_attach = on_attach,
-			capabilities = capabilities,
-			cmd_env = { GOFLAGS = "-tags=integration,integration_external,!integration,e2e" },
-		})
+			-- Server-specific configurations
+			local server_configs = {
+				html = {
+					filetypes = { "html", "templ" },
+				},
+				tailwindcss = {
+					filetypes = { "templ", "javascript", "typescript" },
+					init_options = { userLanguages = { templ = "html" } },
+				},
+				gopls = {
+					cmd_env = { GOFLAGS = "-tags=integration,integration_external,e2e,wireinject" },
+				},
+			}
 
-		lsp_zero.setup_servers(lang_servers)
+			-- Setup servers
+			for _, server in ipairs(servers) do
+				local config = vim.tbl_deep_extend("force", default_config, server_configs[server] or {})
+				lspconfig[server].setup(config)
+			end
 
-		local cmp = require("cmp")
-		local cmp_select = { behavior = cmp.SelectBehavior.Select }
-		cmp.setup({
-			sources = {
-				{ name = "path" },
-				{ name = "nvim_lsp" },
-				{ name = "nvim_lua" },
-				{ name = "luasnip" },
-			},
-			formatting = lsp_zero.cmp_format(),
-			mapping = cmp.mapping.preset.insert({
-				["<CR>"] = cmp.mapping.confirm({ select = true }),
-				["<C-Space>"] = cmp.mapping.complete(),
-				["<S-Tab>"] = cmp.mapping.select_prev_item(cmp_select),
-				["<Tab>"] = cmp.mapping.select_next_item(cmp_select),
-			}),
-		})
-
-		cmp.setup.filetype({ "sql" }, {
-			sources = {
-				{ name = "vim-dadbod-completion" },
-				{ name = "buffer" },
-			},
-		})
-	end,
+			-- Add filetype for templ
+			vim.filetype.add({ extension = { templ = "templ" } })
+		end,
+	},
 }
