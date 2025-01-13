ed7a01a2c1aa6e53347b5e11c75b9dd2a2198340
Axel Almquist <axel.almquist@flowertech.se>
2025-01-13 16:45:38 +0100

    fix

--- nvim/lua/core/set.lua
+++ nvim/lua/core/set.lua
@@ -12,3 +12,14 @@ vim.opt.smartindent = true
 
 vim.opt.updatetime = 50
 vim.opt.clipboard = "unnamedplus"
+
+vim.opt.wrap = false
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
