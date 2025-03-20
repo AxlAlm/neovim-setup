ba6b42067f5914d2271e48bbce12499bcfaba68a
Axel Almquist <accounts@almquist.io>
2025-03-20 08:29:14 +0100

    fix theme

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,8 +1,6 @@
 return {
-	'folke/tokyonight.nvim',
-    config = function() 
-        color = color or "tokyonight"
-        vim.cmd.colorscheme(color)
-        --vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-    end 
+	"folke/tokyonight.nvim",
+	config = function()
+		vim.cmd.colorscheme("tokyonight-moon")
+	end,
 }
