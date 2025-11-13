44cfef5793f33f90ebd7d586fc7002f55cd205a5
Axel Almquist <axel.almquist@flowertech.se>
2025-11-13 10:40:16 +0100

    .templ comment support

--- nvim/lua/plugins/comment.lua
+++ nvim/lua/plugins/comment.lua
@@ -1,8 +1,13 @@
 return {
-		'numToStr/Comment.nvim',
-		config = function()
-            require('Comment').setup {
-                ignore = '^$'
-            }
-		end
+	"numToStr/Comment.nvim",
+	config = function()
+		require("Comment").setup({
+			ignore = "^$",
+			pre_hook = function(ctx)
+				if vim.bo.filetype == "templ" then
+					vim.bo.commentstring = "// %s"
+				end
+			end,
+		})
+	end,
 }
